8271ff27784529608b2f2367570ea8768de78021
Risqi Romadhoni <38126197+risqiromadhoni@users.noreply.github.com>
2024-06-17 18:57:28 +0700

    chore: Update dependencies and fix broken links in App.tsx (#1)

--- packages/mobiles/App.tsx
+++ packages/mobiles/App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import type React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -30,7 +30,7 @@ type SectionProps = PropsWithChildren<{
 }>;
 
 function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = false;
   return (
     <View style={styles.sectionContainer}>
       <Text
