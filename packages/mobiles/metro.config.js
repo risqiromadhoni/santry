7979af6c524d14b88e65cb7d561839f83751824c
Risqi Romadhoni <risqi.romadhoni96@gmail.com>
2024-06-23 19:58:42 +0700

    chore: init expo

--- packages/mobiles/metro.config.js
+++ packages/mobiles/metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
 /**
  * Metro configuration
@@ -7,7 +7,7 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-  watchFolders: [`${__dirname}/../..`],
+	watchFolders: [`${__dirname}/../..`],
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
